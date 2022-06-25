10044b50810fedbad8dfc926fad3ca7dfc79ea02
Sergei Apechuk <ap.serg@gmail.com>
2022-06-25 11:52:57 +0300

    upd

--- shop_project/news/views.py
+++ shop_project/news/views.py
@@ -10,6 +10,6 @@ from django.db.models import Q
 class PostListView(ListView):
     queryset = Post.objects.all()
     context_object_name = 'posts'
-    paginate_by = 9
+    paginate_by = 7
     paginate_orphans = 1
     template_name = 'news.html'
